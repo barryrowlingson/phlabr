c3c490def4564ef70c0f0d7411ec26b19fca340a
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2018-03-09 17:03:29 +0000

    simulate data set

---
+++ R/simulate_datasets.R
@@ -0,0 +1,23 @@
+##' sample independent spatial and temporal data
+##'
+##' etc
+##' @title simulate independent spatial and temporal data
+##' @param N number to simulate
+##' @param time_model a poisson GLM for the temporal model
+##' @param postcode_pop data frame of postcode,pop columns
+##' @return data frame of simulated case postcodes and times
+##' @author Barry Rowlingson
+sim_data <- function(time_model, postcode_pop, timespan=range(time_model$data$Date)){
+
+    times = data.frame(Date = seq(timespan[1],timespan[2],1))
+    times$Total = predict_time_data(times, time_model)
+
+    full = data.frame(Date=rep(times$Date, times$Total))
+    postcode_row = sample(1:nrow(postcode_pop),nrow(full),prob=postcode_pop$pop,replace=TRUE)
+
+    full$postcode = as.character(postcode_pop[postcode_row,]$postcode)
+    full$geometry=st_geometry(st_centroid(postcode_pop[postcode_row,]))
+    full = st_sf(full, sf_column_name="geometry")
+    full
+    
+}
--- R/time_model.R
+++ R/time_model.R
@@ -1,7 +1,11 @@
-fit_time_file <- function(datafile){
+read_time_data <- function(datafile){
     data = read.csv(datafile, stringsAsFactors=FALSE)
     names(data)=c("Total","Year","Date")
     data$Date = as.Date(data$Date, format="%d/%m/%Y")
+    data
+}
+fit_time_file <- function(datafile){
+    data = read_time_data(datafile)
     fit_time_data(data)
 }
 
@@ -13,4 +17,12 @@ fit_time_data <- function(data){
         sin(4*pi*Yearday/365) + cos(4*pi*Yearday/365)
          , data=data, family=poisson)
     m
+}
+
+predict_time_data <- function(data,m){
+    data$Weekday = weekdays(data$Date)
+    data$Yearday = as.POSIXlt(data$Date)$yday
+    f = predict(m, newdata=data, type="link")
+    n = rpois(length(f),lambda=exp(f))
+    n
 }
